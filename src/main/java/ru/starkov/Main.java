164b1acac6a53ecdf78329aaeaf12588ff051c8f
StarkovAleksandr <stanley59rus@gmail.com>
2024-10-20 15:16:41 +0500

    feat: Добавлен класс запуска приложения

--- src/main/java/ru/starkov/Main.java
+++ src/main/java/ru/starkov/Main.java
@@ -1,7 +1,29 @@
 package ru.starkov;
 
+import ru.starkov.infrastructure.impl.LocalFileLoader;
+import ru.starkov.infrastructure.impl.LocalPathFileWriter;
+import ru.starkov.service.UnionFind;
+import ru.starkov.service.ValidStringExtractor;
+
+import java.io.IOException;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final var fileLoader = new LocalFileLoader();
+        final var pathFileWriter = new LocalPathFileWriter();
+        final var invalidStringFilter = new ValidStringExtractor();
+
+        final var file = fileLoader.loadFile(args[0]);
+        final var validStrings = invalidStringFilter.getValidStrings(file);
+
+        final var unionFind = new UnionFind(validStrings);
+        final var groups = unionFind.groupStrings();
+
+        try {
+            pathFileWriter.writeToTxtFile(groups, "output.txt");
+        } catch (IOException e) {
+            System.err.println("Error writing groups to file: " + e.getMessage());
+        }
     }
 }
