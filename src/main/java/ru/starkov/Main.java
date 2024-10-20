051c4f59fd4efaa4aa4f38ef3d519971c5208c08
StarkovAleksandr <stanley59rus@gmail.com>
2024-10-20 22:10:09 +0500

    refactor: улучшена структура Main и добавлена запись времени выполнения

--- src/main/java/ru/starkov/Main.java
+++ src/main/java/ru/starkov/Main.java
@@ -3,27 +3,35 @@ package ru.starkov;
 import ru.starkov.infrastructure.impl.LocalFileLoader;
 import ru.starkov.infrastructure.impl.LocalPathFileWriter;
 import ru.starkov.service.UnionFind;
-import ru.starkov.service.ValidStringExtractor;
+import ru.starkov.service.ValidLinesExtractor;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        final var startTime = LocalTime.now();
+
         final var fileLoader = new LocalFileLoader();
-        final var pathFileWriter = new LocalPathFileWriter();
-        final var invalidStringFilter = new ValidStringExtractor();
+        final var fileWriter = new LocalPathFileWriter();
+        final var validStringExtractor = new ValidLinesExtractor();
 
         final var file = fileLoader.loadFile(args[0]);
-        final var validStrings = invalidStringFilter.getValidStrings(file);
+        final var validNumbers = validStringExtractor.getValidNumbers(file);
 
-        final var unionFind = new UnionFind(validStrings);
-        final var groups = unionFind.groupStrings();
+        final var unionFind = new UnionFind(validNumbers);
+        final var groupedValues = unionFind.groupValues();
 
-        try {
-            pathFileWriter.writeToTxtFile(groups, "output.txt");
-        } catch (IOException e) {
-            System.err.println("Error writing groups to file: " + e.getMessage());
-        }
+        final var endTime = LocalTime.now();
+
+        fileWriter.writeToTxtFile(groupedValues,
+                "Output.txt",
+                Duration.between(startTime, endTime));
+
+        System.out.printf("Job done. Execution time: %d seconds",
+                Duration.between(startTime, LocalTime.now()).get(ChronoUnit.SECONDS));
     }
 }
